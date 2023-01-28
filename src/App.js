313bf4afc8b1a9816b621ec156b6c609d4f9cab2
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-01-28 12:25:58 +0530

    props used and looping using map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 // import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import { expenses } from "./components/expenseData";
 
 function App() {
   return (
-  <div>
-    <h1>Expense Items:</h1>
-  <ExpenseItem></ExpenseItem>
-  </div>
+    <div>
+      <h1>Expense Items:</h1>
+      {expenses.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          location={item.location}
+        ></ExpenseItem>
+      ))}
+
+      {/* <ExpenseItem
+  title = {expenses[1].title}
+  amount = {expenses[1].amount}
+  date = {expenses[1].date}
+  location = {expenses[1].location}>
+  </ExpenseItem>
+
+  <ExpenseItem
+  title = {expenses[2].title}
+  amount = {expenses[2].amount}
+  date = {expenses[2].date}
+  location = {expenses[2].location}>
+  </ExpenseItem>
+
+  <ExpenseItem
+  title = {expenses[3].title}
+  amount = {expenses[3].amount}
+  date = {expenses[3].date}
+  location = {expenses[3].location}>
+  </ExpenseItem> */}
+    </div>
   );
-  }
+}
 export default App;
-
-
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 1, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 249.67;
-    const expenseLocation = 'Mumbai'
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2023, 1, 28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 249.67;
+    // const expenseLocation = 'Mumbai'
 
   return (
     <div className= "expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h2>{expenseLocation}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
