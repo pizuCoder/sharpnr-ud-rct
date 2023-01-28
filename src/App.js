7a222511496a54799df46a6f25fa38824fa76a2b
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-01-28 11:30:12 +0530

    Css styles added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
   <div>
+    <h1>Expense Items:</h1>
   <ExpenseItem></ExpenseItem>
   </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-        <div>
-            <h2>Expense Items</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
-        </div>
-        
-    )
-    }
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+}
 export default ExpenseItem;
