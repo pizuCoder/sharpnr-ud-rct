7a3b0978550885d48be3ef54891b16b0ea8555c8
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-01-28 11:44:54 +0530

    location of expense + js variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 1, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 249.67;
+    const expenseLocation = 'Mumbai'
+
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
+    <div className= "expense-item">
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <h2>{expenseLocation}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
