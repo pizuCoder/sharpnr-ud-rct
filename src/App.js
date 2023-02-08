0013f8726f366663730910a2b51f233431288e7c
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-02-08 12:38:09 +0530

    coded exact same way as trainer still nothing works

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-// import logo from './logo.svg';
-import "./App.css";
-// import ExpenseItem from "./components/ExpenseItem";
-import { expenses } from "./components/Expense/ExpenseData";
-import Expenses from './components/Expense/Expenses';
-import ExpenseForm from "./components/Expense/ExpenseForm";
+// import React, { useEffect } from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
+  const expenses=[
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ]
+  
+
+  function addExpenseHandler(expense) {
+    console.log('In App.js');
+    
+    console.log(expense);
+  }
+  
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Expense Tracker</h2>
-      <ExpenseForm />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
 }
 
 export default App;
-
-
-// return (
-  //   <div>
-  //     <h1>Expense Items:</h1>
-  //     {expenses.map((item) => (
-  //       <ExpenseItem
-  //         key={item.id}
-  //         title={item.title}
-  //         amount={item.amount}
-  //         date={item.date}
-  //         location={item.location}
-  //       ></ExpenseItem>
-  //     ))}
-
-      /* <ExpenseItem
-  title = {expenses[1].title}
-  amount = {expenses[1].amount}
-  date = {expenses[1].date}
-  location = {expenses[1].location}>
-  </ExpenseItem>
-
-  <ExpenseItem
-  title = {expenses[2].title}
-  amount = {expenses[2].amount}
-  date = {expenses[2].date}
-  location = {expenses[2].location}>
-  </ExpenseItem>
-
-  <ExpenseItem
-  title = {expenses[3].title}
-  amount = {expenses[3].amount}
-  date = {expenses[3].date}
-  location = {expenses[3].location}>
-  </ExpenseItem> */
-  //   </div>
-  // );
